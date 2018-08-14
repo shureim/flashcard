fb95edfa38b0b9f095387b560d99d678094c6be7
tarick ali <tarickaliabdi@gmail.com>
2018-08-14 17:25:01 +0300

    created a flashcard webpage

---
+++ js/script.js
@@ -0,0 +1,76 @@
+$().ready(function(){
+  // $("button#green").click(function(){
+  //   // $("body").removeclass();
+  // $("body").addClass("green-background");
+  // });
+  $("button#yellow").click(function() {
+      // $("body").removeclass();
+  $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+      // $("body").removeclass();
+  $("body").addClass("red-background");
+  });
+  $("li#1").click(function(){
+    alert("its a programming language");
+  });
+  $("li#2").click(function(){
+    alert("its a named storage location of a datatype");
+  });
+  $("li#3").click(function(){
+    alert("this is a condion which contain + _ / *");
+  });
+  $("li#4").click(function(){
+    alert("jhfidhhefiuehri");
+  });
+  $("li#5").click(function(){
+    alert("hdgufhujwghvoid");
+  });
+  $("li#6").click(function(){
+    alert("hjfhefhegiuehrrhjyu");
+  });
+  $("li#6").click(function(){
+    alert("jbfiuehfoiqwfvuefhi");
+  });
+  $("li#7").click(function(){
+   alert("jhdighdgsasijwev");
+  });
+  $("li#8").click(function(){
+    alert("jhdgyugfieyuej");
+  });
+  $("li#9").click(function(){
+    alert("jhdgyugfieyuej");
+  });
+  $("li#10").click(function(){
+    alert("jhdgyugfieyuej");
+  });
+  $("li#11").click(function(){
+    alert("jhdgyugfieyuej");
+  });
+  $("li#12").click(function(){
+    alert("jhdgyugfieyuej");
+  });
+  $("li#13").click(function(){
+    alert("jhdgyugfieyuej");
+  });
+  $("li#14").click(function(){
+    alert("jhdgyugfieyuej");
+  });
+  $("li#15").click(function(){
+    alert("jhdgyugfieyuej");
+  });
+  $("li#16").click(function(){
+    alert("jhdgyugfieyuej");
+  });
+  $("li#17").click(function(){
+    alert("jhdgyugfieyuej");
+  });
+  $("li#18").click(function(){
+    alert("jhdgyugfieyuej");
+  });
+  $("li#19").click(function(){
+    alert("jhdgyugfieyuej");
+  });
+
+});
